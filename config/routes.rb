83a96091e8a5ea0bda48c18edff241a3c8dfc712
TraciK <70920574+TraciK@users.noreply.github.com>
2025-08-17 21:27:26 +0000

    add youngest director

--- config/routes.rb
+++ config/routes.rb
@@ -7,7 +7,9 @@ Rails.application.routes.draw do
 
 
   get("/directors", { :controller => "directors", :action => "index"})
+  get("/directors/youngest", { :controller => "directors", :action => "youngest" })
   get("/directors/:path_id", { :controller => "directors", :action => "show" })
+
 
   get("/actors", { :controller => "actors", :action => "index"})
   get("/actors/:path_id", { :controller => "actors", :action => "show" })
