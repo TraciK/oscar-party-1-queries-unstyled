e0149a66453de4b0dabd1fb251a1783aea7ef4e5
TraciK <70920574+TraciK@users.noreply.github.com>
2025-08-17 20:21:51 +0000

    show movies

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,9 @@ Rails.application.routes.draw do
   get("/about", { :controller => "misc", :action => "about" })
 
   get("/movies", { :controller => "movies", :action => "index"})
+  get("/movies/:path_id", { :controller => "movies", :action => "show" })
+
+
   get("/directors", { :controller => "directors", :action => "index"})
   get("/actors", { :controller => "actors", :action => "index"})
 end
