497da1bcebcf0067676d9ce8d2664c93171e1874
TraciK <70920574+TraciK@users.noreply.github.com>
2025-08-17 19:42:29 +0000

    routes set up

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
   get("/", { :controller => "misc", :action => "homepage" })
   get("/about", { :controller => "misc", :action => "about" })
+
+  get("/movies", { :controller => "movies", :action => "index"})
+  get("/directors", { :controller => "directors", :action => "index"})
+  get("/actors", { :controller => "actors", :action => "index"})
 end
