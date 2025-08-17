40cf931e6caebc492e13f71752e846d1fa8d30e5
TraciK <70920574+TraciK@users.noreply.github.com>
2025-08-17 21:05:02 +0000

    add director pages

--- config/routes.rb
+++ config/routes.rb
@@ -7,6 +7,7 @@ Rails.application.routes.draw do
 
 
   get("/directors", { :controller => "directors", :action => "index"})
-  
+  get("/directors/:path_id", { :controller => "directors", :action => "show" })
+
   get("/actors", { :controller => "actors", :action => "index"})
 end
